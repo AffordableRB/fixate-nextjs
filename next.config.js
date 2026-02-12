23426206f6e590b53c62fcf7802c79f391ded9bd
AffordableRB <1leightonjay@gmail.com>
2026-02-12 10:25:11 -0800

    Enable static export for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  output: 'export',
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
